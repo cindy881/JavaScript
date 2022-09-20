750eb3ac0082754fa0305ae7e453e931daf147bc
cindy881 <cindy.19039@mhs.unesa.ac.id>
2022-09-20 21:09:42 +0700

    Tugas 6 Javascript

--- js/tugas5.js
+++ js/tugas5.js
@@ -1,31 +1,34 @@
-function hitung() {
+function hitung(operasi) {
     var frm = document.getElementById('calcForm');
     var a1 = parseFloat(frm.angka1.value);
     var a2 = parseFloat(frm.angka2.value);
-    var operasi = frm.tombol.value;
 
-    switch (operasi) {
-        case 'btn_tambah':
-            if (isNaN(a1) || isNaN(a2)) {
-                alert("Harap masukkan angka!!");
-            }    
-            else {
-                var total = a1 + a2; //rumus
-                frm.hasil.value = total; //penempatan hasil 
-            } 
-            break;
-        case 'btn_kurang':
-            if (isNaN(a1) || isNaN(a2)) {
-                alert("Harap masukkan angka!!");
-            }    
-            else {
-                var total = a1 - a2; //rumus
-                frm.hasil.value = total; //penempatan hasil 
-            } 
-            break;
-    
-        default:
-            break;
+    if (isNaN(a1) || isNaN(a2)) {
+        alert("Harap masukkan angka!!");
+    } else {
+        switch (operasi) {
+            case "tambah":
+                var total = a1 + a2;
+                break;
+            case "kurang": 
+                var total = a1 - a2; 
+                break;
+            case "kali": 
+                var total = a1 * a2; 
+                break;
+            case "bagi": 
+                var total = a1 / a2; 
+                break;
+            case "pangkat": 
+                var total = Math.pow(a1,a2); 
+                break;
+        
+            default:
+                var total = 0;
+                break;
+        }
     }
+
+    frm.hasil.value = total;
         
 }
